3c6509bcb498acb14779efc9087dad900044548e
Mirko Groß <nickless85@outlook.de>
2018-03-14 19:40:29 +0100

    Alle Anteile die im Praxis-Beispiel entwickelt werden sollen entfernt

--- app/src/main/java/com/example/mirko/todo_liste/MainActivity.java
+++ app/src/main/java/com/example/mirko/todo_liste/MainActivity.java
@@ -2,15 +2,10 @@ package com.example.mirko.todo_liste;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.SparseBooleanArray;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -18,9 +13,6 @@ public class MainActivity extends Activity {
     Button btnDelete;
     Button btnAdd;
     EditText etInput;
-    ArrayAdapter<ListEntry> adapter;
-    List<ListEntry> lvMainData;
-    ListEntryDBHelper listEntryDBHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,37 +23,17 @@ public class MainActivity extends Activity {
         this.btnAdd = findViewById(R.id.btnAdd);
         this.btnDelete = findViewById(R.id.btnDelete);
         this.etInput = findViewById(R.id.etInput);
-
-        this.lvMainData = new ArrayList<>();
-        this.adapter = new ArrayAdapter<ListEntry>(this, R.layout.list_item, lvMainData);
-        this.lvMain.setAdapter(adapter);
-        this.lvMain.setItemsCanFocus(false);
-
-        this.lvMain.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-        this.initEvents();
-
-        this.listEntryDBHelper = new ListEntryDBHelper(this);
-        this.listEntryDBHelper.open();
-
-        this.fillVisualComponents();
     }
 
     private void fillVisualComponents(){
-        this.adapter.addAll(listEntryDBHelper.getAllEntries());
-        this.adapter.notifyDataSetChanged();
+
     }
 
     private void initEvents(){
         this.btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String inputText = etInput.getText().toString().trim();
-                if(inputText.length() > 0){
-                    ListEntry listEntry = new ListEntry(null,inputText);
-                    lvMainData.add(listEntry);
-                    adapter.notifyDataSetChanged();
-                    listEntryDBHelper.saveEntry(listEntry);
-                }
+
             }
         });
 
@@ -69,18 +41,6 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View view) {
 
-                SparseBooleanArray checkedItemPositions = lvMain.getCheckedItemPositions();
-
-                for(int i = lvMainData.size()-1; i >= 0 ;i--){
-                    if(checkedItemPositions.get(i)){
-                        ListEntry listEntry = lvMainData.get(i);
-                        if(listEntryDBHelper.removeListEntry(listEntry)){
-                            lvMain.setItemChecked(i,false);
-                            lvMainData.remove(i);
-                            adapter.notifyDataSetChanged();
-                        }
-                    }
-                }
             }
         });
     }
